64e79ef95a8cf959c227363d3b394f4e1624a84f
Diego Almeida <diegoalmda@gmail.com>
2023-01-21 14:44:08 -0300

    mobile: API to check habits and update progress

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,11 +4,15 @@ import { appRoutes } from './routes'
 
 const app = Fastify()
 
-app.register(cors)
+app.register(cors, {
+  origin: true,
+})
+
 app.register(appRoutes)
 
 app.listen({
-  port: 3333
-}).then(() => {
-  console.log("HTTP Server is running on port 3333!")
+  port: 3333,
+  host: '0.0.0.0',
+}).then((url) => {
+  console.log(`HTTP Server is running on ${url}!`)
 })
